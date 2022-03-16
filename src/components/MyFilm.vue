4e4586135ed7c1ec9c841fb655f92bbdc6e0f324
stefanom13 <stefanomancuso13@gmail.com>
2022-03-16 18:04:17 +0100

    milestone 3 completa

--- src/components/MyFilm.vue
+++ src/components/MyFilm.vue
@@ -3,6 +3,10 @@
       <div class="listFind">
           <ul>
               <li v-for="film in filmList" :key="film.id">
+                  <figure>
+                      <img v-if="film.backdrop_path" :src="`https://image.tmdb.org/t/p/w342${film.backdrop_path}`">
+                      <img v-else width="342" height="192" src="https://eapp.org/wp-content/uploads/2018/05/poster_placeholder.jpg">
+                  </figure>
                   <h3>Film</h3>
                   <p>Titolo:{{film.title}}</p>
                   <p>Titolo originale: {{film.original_title}}</p>
--- src/components/MySerietv.vue
+++ src/components/MySerietv.vue
@@ -4,8 +4,12 @@
           <ul>
               <li v-for="serie in serieTv" :key="serie.id">
                   <h3>Serie Tv</h3>
+                  <figure>
+                      <img v-if="serie.backdrop_path" :src="`https://image.tmdb.org/t/p/w342${serie.backdrop_path}`">
+                      <img v-else width="342" height="192" src="https://eapp.org/wp-content/uploads/2018/05/poster_placeholder.jpg">
+                  </figure>
                   <p>Titolo:{{serie.name}}</p>
-                  <p>Titolo originale: {{serie.original_title}}</p>
+                  <p>Titolo originale: {{serie.original_name}}</p>
                   <p>Lingua originale: {{serie.original_language}}</p>
                   <p>Voto: {{serie.vote_average}}</p>
               </li>
